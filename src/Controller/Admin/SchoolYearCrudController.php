584e7003e24cc5ef3dba735a66455f18c0b4fdaa
Saka <simon.ponitzki@gmail.com>
2021-02-17 17:30:49 +0100

    easyadmin

---
+++ src/Controller/Admin/SchoolYearCrudController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\SchoolYear;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class SchoolYearCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return SchoolYear::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('name'),
+            DateField::new('dateStart')
+            ->setFormTypeOptions([
+                'widget' => 'single_text',
+                // 'html5' => false,
+                // 'format' => 'dd/MM/yyyy',
+            ]),
+            DateField::new('dateEnd')
+            ->setFormTypeOptions([
+                'widget' => 'single_text',
+                // 'html5' => false,
+                // 'format' => 'dd/MM/yyyy',
+            ]),
+            AssociationField::new('users')
+                ->setFormType(EntityType::class)
+                ->setFormTypeOptions([
+                    'choice_label' => function($user) {
+                        return "{$user->getFirstname()} {$user->getLastname()} ({$user->getId()})";
+                    }
+                    ]),
+        ];
+    }
+    
+}
